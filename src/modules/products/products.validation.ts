d8cb8a7037bd3637c20e495c158020989394d98f
akilinjamam <akilinjamam@gmail.com>
2025-11-08 00:21:46 +0600

    product-model-updated

--- src/modules/products/products.validation.ts
+++ src/modules/products/products.validation.ts
@@ -13,7 +13,7 @@ export const productSchema = z.object({
 	color: z.string(),
 	purchase: z.number().nonnegative(),
 	salesPrice: z.number().nonnegative(),
-	discount: z.number().min(0).max(100),
+	discount: z.number().min(0),
 	quantity: z.number().int().nonnegative(),
 	sold: z.number().int().nonnegative().optional(),
 	features: z.array(z.string()).optional(),
