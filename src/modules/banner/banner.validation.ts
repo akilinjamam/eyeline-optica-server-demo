e645395a9714ab0835f8aadc6faf3445e2126b18
akilinjamam <akilinjamam@gmail.com>
2025-11-14 03:46:32 +0600

    banner-updated

--- src/modules/banner/banner.validation.ts
+++ src/modules/banner/banner.validation.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const bannerSchema = z.object({
 	category: z.string().trim().min(1, "Category cannot be empty"),
 
-	images: z.array(z.string()).nonempty("At least one image is required"),
+	images: z.array(z.string()).nonempty("At least one image is required now"),
 });
 
 // ✅ Inferred Type (useful in controllers)
