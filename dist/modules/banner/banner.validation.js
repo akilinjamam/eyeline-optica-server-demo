634a29f74df7e94e2ec0c057e3d3e87d26a205d0
akilinjamam <akilinjamam@gmail.com>
2025-11-14 03:52:58 +0600

    banner-validation-added

--- dist/modules/banner/banner.validation.js
+++ dist/modules/banner/banner.validation.js
@@ -4,6 +4,6 @@ exports.bannerSchema = void 0;
 const zod_1 = require("zod");
 exports.bannerSchema = zod_1.z.object({
     category: zod_1.z.string().trim().min(1, "Category cannot be empty"),
-    images: zod_1.z.array(zod_1.z.string()).nonempty("At least one image is required now"),
+    images: zod_1.z.array(zod_1.z.string().url("Invalid image URL")),
 });
 //# sourceMappingURL=banner.validation.js.map
--- src/modules/banner/banner.validation.ts
+++ src/modules/banner/banner.validation.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const bannerSchema = z.object({
 	category: z.string().trim().min(1, "Category cannot be empty"),
 
-	images: z.array(z.string()).nonempty("At least one image is required now"),
+	images: z.array(z.string().url("Invalid image URL")),
 });
 
 // ✅ Inferred Type (useful in controllers)
