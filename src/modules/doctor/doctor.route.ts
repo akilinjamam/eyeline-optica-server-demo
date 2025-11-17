ca3a2ec6527207ad061b9ce3256190e4b335910f
akilinjamam <akilinjamam@gmail.com>
2025-11-17 16:42:44 +0600

    doctor-updated

--- src/modules/doctor/doctor.route.ts
+++ src/modules/doctor/doctor.route.ts
@@ -2,7 +2,6 @@ import express from "express";
 import { upload } from "../../app/middleware/multer";
 import combineImagesWithTextData from "../../app/middleware/combineImagesWithData";
 import { doctorController } from "./doctor.controller";
-import { protect } from "../../app/middleware/auth";
 
 const doctorRouter = express.Router();
 
@@ -13,7 +12,7 @@ doctorRouter.post(
 	doctorController.createDoctorsController
 );
 
-doctorRouter.get("/", protect, doctorController.getAllDoctorsController);
+doctorRouter.get("/", doctorController.getAllDoctorsController);
 doctorRouter.get("/:email", doctorController.getSingleDoctorController);
 doctorRouter.put(
 	"/update-doctor/:id",
