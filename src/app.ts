013464d8ffaded46404ef7940c815b6be8a0f11f
akilinjamam <akilinjamam@gmail.com>
2026-02-16 15:08:00 +0600

    app.ts-updated

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ app.use(
 	cors({
 		origin: [
 			"http://localhost:5173",
+			"https://eyeline-optica-client-demo.vercel.app",
 			"https://eyelineoptica.com",
 			"https://www.eyelineoptica.com",
 			"https://cms.eyelineoptica.com",
