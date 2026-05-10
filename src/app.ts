850bec8a500a2682e44862be88cbc57ab03645fc
akilinjamam <akilinjamam@gmail.com>
2026-05-11 01:34:05 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use(
 );
 
 app.get("/", (req: Request, res: Response) => {
-	res.send("Hello, TypeScript + Express + MONGOOSE..!");
+	res.send("Hello, TypeScript + Express + MONGOOSE..! demo");
 });
 
 app.use("/api/v1/", router);
